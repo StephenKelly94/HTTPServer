57a9da47bb564e0abe1d87b0b87fa33ef72856ba
Paulius <paulius.sereiva@gmail.com>
2015-03-11 13:08:58 +0100

    Step 13: Welcome files

--- TCPEchoServer/TCPEchoServer/HandlingRequest.cs
+++ TCPEchoServer/TCPEchoServer/HandlingRequest.cs
@@ -12,7 +12,19 @@ namespace TCPEchoServer
         public HTTPResponse HandleRequest(HTTPRequest request)
         {
             HTTPResponse response;
-            if (File.Exists(ServerStart.RootCatalog + request.FilePath))
+            if (request.FilePath == "/")
+            {
+                String indexString = "/index.html";
+                FileStream fileStream = new FileStream(ServerStart.RootCatalog + indexString, FileMode.Open);
+                ContentTypes contentTypes = new ContentTypes();
+                response = new HTTPResponse("HTTP/1.0 200 OK\r\n",
+                    "Date: " + string.Format("{0:yyyy-MM-dd_hh-mm-ss-tt}", DateTime.Now) + "\r\n",
+                    "",
+                    "Content-Length: " + fileStream.Length + "\r\n\r\n",
+                    indexString);
+                fileStream.Close();
+            }
+            else if (File.Exists(ServerStart.RootCatalog + request.FilePath))
             {
                 FileStream fileStream = new FileStream(ServerStart.RootCatalog + request.FilePath, FileMode.Open);
                 ContentTypes contentTypes = new ContentTypes();
